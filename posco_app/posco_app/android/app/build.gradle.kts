80d4a31a2b0f0796a5fb8156823929c1e6460f62
Mitra Zulkia <mitrazulkia84850@gmail.com>
2026-06-22 23:30:19 +0700

    feat: initialize Android build configuration with gradle.properties and build.gradle.kts

--- posco_app/posco_app/android/app/build.gradle.kts
+++ posco_app/posco_app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.posco_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
