7725a5328535509d97477f4968f37295342d9e0a
Mitra Zulkia <mitrazulkia84850@gmail.com>
2026-06-22 23:11:52 +0700

    feat: add Config class to manage environment-based API base URLs

--- posco_app/posco_app/lib/shared/config.dart
+++ posco_app/posco_app/lib/shared/config.dart
@@ -5,9 +5,11 @@ class Config {
   static String get apiBaseUrl {
     if (!kReleaseMode) {
       if (!kIsWeb && Platform.isAndroid) {
-        return 'http://10.0.2.2:4000';
+        return 'http://10.0.2.2:4000'; // Untuk emulator Android lokal
       }
+      return 'http://localhost:4000'; // Untuk browser/iOS lokal
     }
-    return 'http://localhost:4000';
+    // API URL Production di Vercel
+    return 'https://posco-backend.vercel.app';
   }
 }
